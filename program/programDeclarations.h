5a8724474a1d111eb2cf08575208132b1eb6a8a1
1081582 <1081582@hr.nl>
2026-03-11 14:48:15 +0100

    Updated hello world with void pointer examples

--- program/programDeclarations.h
+++ program/programDeclarations.h
@@ -1,13 +1,15 @@
 #pragma once //Prevents header from being implemented more then once. https://en.cppreference.com/w/cpp/preprocessor/impl
 
+#define NL printf("\n")
+
 // Operation
 /*
 The idea is to use void pointers and structs.
 */
 
 /*Updates the sensors.*/
-void sensorRoutine(void* pointer);
+void sensorRoutine();
 /*Checks if hardware interupts occured.*/
-void physicalInputRoutine(void* pointer);
+void physicalInputRoutine();
 /*Used to control the motors*/
-void motorRoutine(void* pointer);
+void motorRoutine();
