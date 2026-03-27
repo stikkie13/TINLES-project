b7b0310ec0830c843bcaf7c3772fb22ff5604db7
1081582 <1081582@hr.nl>
2026-03-27 12:07:48 +0100

    Not bad

--- program/QuadCopterSim/src/pidController.h
+++ program/QuadCopterSim/src/pidController.h
@@ -63,7 +63,9 @@ void controller_p_acro(double duty_cycle[4], double sticks[4], double gyro[3], d
     {
         duty_cycle[i] = rollCommand * rollMask[i] + pitchCommand * pitchMask[i];
     }
-    printf("test\n");
+    printf("roll:%f\t", gyro[0]);
+    printf("pitch:%f\t", gyro[1]);
+    printf("\n");
 }
 
 #endif
